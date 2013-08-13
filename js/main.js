23ddd0316fc8e8dc3315821181034bc8857a6c31
David Zuckerman <davidztlv@gmail.com>
2013-08-13 18:06:04 +0300

    updated with mobile version, and using LESS for css

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
 var scrollTiming = 1500;
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
 
-$(document).ready(function() {
-    $("#everest-nav").click(function (event) {
-        $.scrollTo("#everest", {
-            duration: scrollTiming
+$(document).ready(function () {
+
+    if(!isMobile) {
+        $("#mobile-photo-grid").remove();
+        //set up our click handlers
+        $(".everest-nav").click(function (event) {
+            stopListening();
+            $.scrollTo("#everest", {
+                duration: scrollTiming,
+                onAfter: resumeListening
+            });
+            return false;
         });
-        return false;
-    });
-    $("#tlv-nav").click(function (event) {
-        $.scrollTo("#tlv", {
-            duration: scrollTiming
+        $(".tibet-nav").click(function (event) {
+            stopListening();
+            $.scrollTo("#tibet", {
+                duration: scrollTiming,
+                onAfter: resumeListening
+            });
+            return false;
         });
-        return false;
-    });
+        $(".tlv-nav").click(function (event) {
+            stopListening();
+            $.scrollTo("#tlv", {
+                duration: scrollTiming,
+                onAfter: resumeListening
+            });
+            return false;
+        });
+    } else {
+       $('.photoset-grid-basic').photosetGrid({
+           highresLinks: true, 
+           gutter: '2px',
+           onComplete: function(){
+                // Show the grid after it renders
+                $('.photoset-grid-basic').attr('style', '');
+                $('.photoset-grid-basic a').colorbox({
+                  photo: true,
+                  scalePhotos: true,
+                  maxHeight:'90%',
+                  maxWidth:'90%'
+                });
+            }   
+           
+       });
+    }
 });
